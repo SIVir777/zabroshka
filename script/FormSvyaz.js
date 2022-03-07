ac0def1b092b103f890b1905bbf0f35880edc300
SIVir777 <94316616+SIVir777@users.noreply.github.com>
2022-03-07 13:37:31 +0300

    Add files via upload

---
+++ script/FormSvyaz.js
@@ -0,0 +1,56 @@
+const knop = document.querySelector('.hover-knop');
+const modal = document.querySelector('.modal-okno-parent');
+const krestik = document.querySelector('.krestik');
+const card = document.querySelector('.modal-okno');
+
+const form = document.querySelector('#form-data');
+const knopCard = document.querySelector('#knop-modal-okna');
+const userName = document.querySelector('.uName');
+const email = document.querySelector('.e-adres');
+const message = document.querySelector('.pole-soob');
+
+knop.addEventListener('click', function() {
+    modal.classList.remove('hidden-modal');
+    document.body.style.overflow = 'hidden';
+});
+krestik.addEventListener('click', function() {
+    modal.classList.add('hidden-modal');
+    document.body.style.overflow = '';
+});
+modal.addEventListener('click', function() {
+    modal.classList.add('hidden-modal');
+    document.body.style.overflow = '';
+});
+card.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    userName.value = '';
+    email.value = '';
+    message.value = '';
+    modal.classList.add('hidden-modal');
+    const objectData = Object.fromEntries(formData.entries());
+    console.log(objectData);
+    socket.send(JSON.stringify(objectData));
+});
+
+const socket = new WebSocket('ws://git.heroku.com/zabroshka.git/datauser');
+
+socket.onopen = function() {
+
+};
+
+socket.onmessage = function() {
+
+};
+
+socket.onerror = function() {
+    console.log('WebSocket error');
+};
+
+socket.onclose = function() {
+
+};
